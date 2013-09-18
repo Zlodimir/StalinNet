f178ca7df0028340822befa99826353653cd2472
Zlodimir <sslupinos@gmail.com>
2013-09-18 11:37:06 +0400

    еще

--- index.rb
+++ index.rb
@@ -2,6 +2,15 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  erb :index
+	erb :index
 end
 
+get '/За_батьку_нашего_Путина!!!' do
+	erb :index
+	@msg = 'ЗА БАТЬКУ ПУТИНА!!!'
+end 
+
+get '/За_батьку_нашего_Сталина!!!' do
+	erb :index
+	@msg = 'ЗА БАТЬКУ СТАЛИНА!!!'
+end 
