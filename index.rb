46e4b2a35b33011d277bb76192cad81eee28b2eb
Zlodimir <sslupinos@gmail.com>
2013-09-19 12:10:26 +0400

    комит

--- index.rb
+++ index.rb
@@ -1,16 +1,6 @@
-require 'rubygems'
-require 'sinatra'
+require rubygems
+require sinatra
 
 get '/' do
 	erb :index
 end
-
-get '/Za_batky_nashego_putina' do
-	erb :index
-	#@msg = 'ЗА БАТЬКУ ПУТИНА!!!'
-end 
-
-get '/За_батьку_нашего_Сталина!!!' do
-	erb :index
-	
-end 
