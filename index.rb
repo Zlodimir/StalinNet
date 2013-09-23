be87be84d0d724822761dce3d4c3d0759b97b9fc
Zlodimir <sslupinos@gmail.com>
2013-09-23 14:20:29 +0400

    3

--- index.rb
+++ index.rb
@@ -10,7 +10,7 @@ get '/Za_batky_nashego_putina' do
 	erb :index
 end 
 
-get '/За_батьку_нашего_Сталина!' do
-	@msg = 'За батьку нашего Сталина'
+get '/Za_batky_nashego_Stalina!' do
+	@msg = 'За батьку нашего Сталина!'
 	erb :index
 end
