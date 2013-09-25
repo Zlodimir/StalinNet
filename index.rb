e59b5b8012ffa1a6fd9cd724332a4c3a04e576a4
Zlodimir <sslupinos@gmail.com>
2013-09-25 14:14:55 +0400

    datamapper recources

--- index.rb
+++ index.rb
@@ -7,9 +7,22 @@ require 'dm-postgres-adapter'
 
 
 # If you want the logs displayed you have to do this before the call to setup
-DataMapper::Logger.new($stdout, :debug)
+#DataMapper::Logger.new($stdout, :debug)
 # A Postgres connection:
 DataMapper.setup(:default, 'postgres://net_stalin:123net456stalin@localhost/net_stalin')
+
+class Vote
+  include DataMapper::Resource
+
+  property :id,         Serial    # An auto-increment integer key
+  property :init_date,  DateTime  # init time
+  property :ip,       	String    # Ip Address
+  property :host, 		String    # Host name
+  property :browser		String	  # Browser
+end
+
+DataMapper.finalize
+
 
 get '/' do
 	erb :index
